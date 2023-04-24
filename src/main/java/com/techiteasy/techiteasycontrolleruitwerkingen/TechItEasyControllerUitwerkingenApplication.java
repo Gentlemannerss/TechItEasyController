f5da192a9868206f6b4feceb72525f7ffbd6db51
Gentlemannerss <damonvanbergen@gmail.com>
2023-04-24 12:58:20 +0200

    first commit

---
+++ src/main/java/com/techiteasy/techiteasycontrolleruitwerkingen/TechItEasyControllerUitwerkingenApplication.java
@@ -0,0 +1,13 @@
+package com.techiteasy.techiteasycontrolleruitwerkingen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasyControllerUitwerkingenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechItEasyControllerUitwerkingenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techiteasy/techiteasycontrolleruitwerkingen/TechItEasyControllerUitwerkingenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techiteasy.techiteasycontrolleruitwerkingen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyControllerUitwerkingenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
